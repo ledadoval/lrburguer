36236a5828be80020c131625f76966d0a110a79c
Leda <ledadoval2002@gmail.com>
2025-01-21 10:13:35 -0300

    removing lorem and add images

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById('mobile-menu-button').addEventListener('click', () => {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+  });
+  
